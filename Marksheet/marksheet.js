a92b37a8e01dbe9ef35a1edd290883514f0aeec4
Shayan <162302193+Shayan123458@users.noreply.github.com>
2024-03-27 11:05:40 +0500

    Add files via upload

---
+++ Marksheet/marksheet.js
@@ -0,0 +1,16 @@
+var percentage = 20;
+if (percentage >= 90 && percentage < 100) {
+    console.log("A++ Grade");
+}
+else if (percentage >= 80 && percentage <= 89.99) {
+    console.log("A+ Grade");
+}
+else if (percentage >= 70 && percentage <= 79.99) {
+    console.log("A Grade");
+}
+else if (percentage >= 60 && percentage <= 69.99) {
+    console.log("B Grade");
+}
+else {
+    console.log("Fail");
+}
